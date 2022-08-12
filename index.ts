2ea40d3ada8e91caed0969590874e5c445a94869
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2022-08-13 00:07:40 +0300

    Fix stuffs and things

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+#! /usr/bin/env node
+
 import { program } from 'commander';
 import { handleOauthCallback } from './callback';
 import { completeOauthChallenge, generateOauthChallenge } from './challenge';
